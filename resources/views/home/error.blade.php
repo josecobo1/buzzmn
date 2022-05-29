7e8fc04ca4d58ae19e101f7bd8cf67b412cc2791
Jose <dev1@motorvinilo.com>
2022-05-29 19:05:27 +0200

    Se han mejorado los estilos del componente que informe de un error durante el proceso de consulta del tiempo

--- resources/views/home/error.blade.php
+++ resources/views/home/error.blade.php
@@ -1,7 +1,10 @@
 <div class="row">
-    <div class="col">
-        <p class="h1">
-            Se ha producido un error al contactar con la api.
+    <div class="col text-center text-danger">
+        <p class="h3">
+            Se ha producido un error al consultar el tiempo.
+        </p>
+        <p class="h3">
+            Comprueba que el código postal es correcto o vuelve a intentarlo más tarde.
         </p>
     </div>
 </div>
